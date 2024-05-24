d5f2fa4e59233c0fb85b52914486736784135344
tommy johnson <tommy.johnson.dev@gmail.com>
2024-05-25 00:40:33 +0900

    build upload

--- src/components/Grid.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-const Grid = styled.div`
-    display:flex;
-    flex-direction:column;
-`
-export default Grid;
--- src/utils/constants.tsx
+++
@@ -1,1 +0,0 @@
-module.exports = { bnb: "BNB", eth: "ETH" };
